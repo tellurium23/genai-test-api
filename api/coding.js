b0ff09acb32305b004c378d5e984f92407879901
tellurium23 <1nf1n11te.cry@gmail.com>
2026-05-23 07:37:59 +0900

    CORS対応を追加

--- api/coding.js
+++ api/coding.js
@@ -5,6 +5,14 @@ const client = new OpenAI({
 });
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   const processingStartedAt = new Date().toISOString();
 
   try {
